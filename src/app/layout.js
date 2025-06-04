e5beb4222e4b0bb5fd6b0638e99a6cb58650a360
devnazeer <nazeerahmed324680@gmail.com>
2025-06-05 02:13:30 +0500

    checking

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,13 @@
-import ClientLayoutWrapper from "@/Components/ClientLayoutWrapper";
 import { Roboto } from "next/font/google";
-import "./globals.css";
 import Sidebar from "@/Components/Sidebar";
 import { Suspense } from "react";
 import Loading from "./Loading";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+import { LanguageProvider } from "@/Components/LanguageProvider";
+import theme from "@/theme";
+import { CssBaseline } from "@mui/material";
+import "./globals.css";
 
 const roboto = Roboto({
   weight: ["300", "400", "500", "700"],
@@ -19,9 +23,17 @@ export default function RootLayout({ children, params }) {
     <html lang={locale} data-arp="" className={roboto.className}>
       <body inmaintabuse="1" style={{ margin: "unset", fontFamily: "roboto" }}>
         <Suspense fallback={<Loading />}>
-          <ClientLayoutWrapper>
-            <Sidebar>{children}</Sidebar>
-          </ClientLayoutWrapper>
+          <LanguageProvider>
+            <AppRouterCacheProvider
+              options={{ key: "css", enableCssLayer: true }}
+            >
+              <ThemeProvider theme={theme}>
+                <CssBaseline />
+                {/* {children} */}
+                <Sidebar>{children}</Sidebar>
+              </ThemeProvider>
+            </AppRouterCacheProvider>
+          </LanguageProvider>
         </Suspense>
       </body>
     </html>
